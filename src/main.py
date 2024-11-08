ad4077cd952c58f781f57d535f703e9a47acdc8a
Sayan Bharadwaj <sayanbharadwaj03@gmail.com>
2024-11-08 22:38:01 +0000

    implement hunting function (#7)
    Co-authored-by: Ed Birchall <edbirchy664+gh@gmail.com>

--- src/main.py
+++ src/main.py
@@ -47,6 +47,43 @@ def calculateAgeBasedMortality(age: int) -> float:
         return 0.08 * exp(2.47 * (age - 16))
 
 
+
+def count_group(population, age=None, min_age=None, countMale=True, countFemale=False):
+    if age is not None:
+        # Count individuals of a specific age
+        return [ind for ind in population if ind.age == age and (ind.isMale==countMale or ind.isFemale==countFemale)]
+    elif min_age is not None:
+        # Count individuals older than a certain age
+        return [ind for ind in population if ind.age > min_age and (ind.isMale==countMale or ind.isFemale==countFemale)]
+    return []
+
+def hunting(population: List[Deer],
+            params: ModelParameters, 
+            huntingStrategy: HuntingParameters):
+    calves=count_group(population, age= 0, countMale=True, countFemale=True)
+    if len(calves)>params.huntingLimit:
+        calves=calves[huntingStrategy.calves:]
+
+    youngHinds=count_group(population, age= 1, countMale=False, countFemale=True)
+    if len(youngHinds)>params.huntingLimit:
+        youngHinds=youngHinds[huntingStrategy.youngHinds:]
+
+    youngStags=count_group(population, age= 1, countMale=True, countFemale=False)
+    if len(youngStags)>params.huntingLimit:
+        youngStags=youngStags[huntingStrategy.youngStags:]
+
+    hinds=count_group(population, min_age= 1, countMale=False, countFemale=True)
+    if len(hinds)>params.huntingLimit:
+        hinds=hinds[huntingStrategy.matureHinds:]
+
+    stags=count_group(population, min_age= 1, countMale=True, countFemale=False)
+    if len(stags)>params.huntingLimit:
+        stags=stags[huntingStrategy.matureStags:]
+    
+    population=calves+youngHinds+youngStags+hinds+stags
+    return population
+
+ 
 def adjustMortalityRate(base_mortality: float, max_cap_impact: float, cap_curve_slope: float, inow: int, imax: int) -> float:
     # Adjust the mortality rate based on carrying capacity
     adjustment = (max_cap_impact / 2) * (1 + tanh(cap_curve_slope * (inow - imax)))
@@ -75,7 +112,6 @@ def naturalDeath(population: List[Deer], params: ModelParameters):
             survivors.append(deer)  # Deer survives if random number >= adjusted mortality
     
     return survivors
-
 
 class Deer():
     def __init__(

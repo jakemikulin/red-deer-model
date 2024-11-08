d323c30d6fd8323d5cd0d89b83c81600040ff2df
Sayan Bharadwaj <sayanbharadwaj03@gmail.com>
2024-11-08 18:21:48 +0000

    implement hunting function

--- src/main.py
+++ src/main.py
@@ -40,12 +40,40 @@ def reproduce(population: List[Deer], params: ModelParameters):
 def naturalDeath(population: List[Deer], params: ModelParameters):
     pass
 
+def count_group(population, age=None, min_age=None, countMale=True, countFemale=False):
+    if age is not None:
+        # Count individuals of a specific age
+        return [ind for ind in population if ind.age == age and (ind.isMale==countMale or ind.isFemale==countFemale)]
+    elif min_age is not None:
+        # Count individuals older than a certain age
+        return [ind for ind in population if ind.age > min_age and (ind.isMale==countMale or ind.isFemale==countFemale)]
+    return []
 
 def hunting(population: List[Deer],
             params: ModelParameters, 
             huntingStrategy: HuntingParameters):
-    pass
+    calves=count_group(population, age= 0, countMale=True, countFemale=True)
+    if len(calves)>params.huntingLimit:
+        calves=calves[huntingStrategy.calves:]
 
+    youngHinds=count_group(population, age= 1, countMale=False, countFemale=True)
+    if len(youngHinds)>params.huntingLimit:
+        youngHinds=youngHinds[huntingStrategy.youngHinds:]
+
+    youngStags=count_group(population, age= 1, countMale=True, countFemale=False)
+    if len(youngStags)>params.huntingLimit:
+        youngStags=youngStags[huntingStrategy.youngStags:]
+
+    hinds=count_group(population, min_age= 1, countMale=False, countFemale=True)
+    if len(hinds)>params.huntingLimit:
+        hinds=hinds[huntingStrategy.matureHinds:]
+
+    stags=count_group(population, min_age= 1, countMale=True, countFemale=False)
+    if len(stags)>params.huntingLimit:
+        stags=stags[huntingStrategy.matureStags:]
+    
+    population=calves+youngHinds+youngStags+hinds+stags
+    return population
 
 class Deer():
     def __init__(
